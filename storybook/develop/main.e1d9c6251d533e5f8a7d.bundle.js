87f1c0938b4ac26695748f8c7100675e5fcac4ba
Tomasz Mittlener <tomasz.mittlener@schibsted.com>
2020-07-31 11:05:12 +0200

    feat: cleanup storybook pages

--- storybook/develop/main.e1d9c6251d533e5f8a7d.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{119:function(module,exports,__webpack_require__){__webpack_require__(120),module.exports=__webpack_require__(292)},189:function(module,exports){}},[[119,1,2]]]);
-//# sourceMappingURL=main.e1d9c6251d533e5f8a7d.bundle.js.map
