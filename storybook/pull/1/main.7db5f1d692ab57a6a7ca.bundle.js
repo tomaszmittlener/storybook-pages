b842d1b490ea9b82586cd37045128cac85189d55
tomaszmittlener <tomaszmittlener@users.noreply.github.com>
2020-07-31 09:16:30 +0000

    Remove storybook for pull/1.

--- storybook/pull/1/main.7db5f1d692ab57a6a7ca.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1257:function(n,o,c){"use strict";c.r(o);c(1258),c(1263),c(1294)},432:function(n,o,c){c(433),c(580),n.exports=c(1257)},497:function(n,o){}},[[432,1,2]]]);
